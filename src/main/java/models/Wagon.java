408fd82f3558c5f6709accfdc294676367542847
crash <raymond.borghoff@hva.nl>
2020-09-19 19:20:11 +0200

    alles tot 5 af

--- src/main/java/models/Wagon.java
+++ src/main/java/models/Wagon.java
@@ -52,7 +52,10 @@ public abstract class Wagon {
      * @return  whether this wagon has a wagon prepended at the front
      */
     public boolean hasPreviousWagon() {
-        return !(this.previousWagon == null);
+        if (this.previousWagon != null){
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -119,8 +122,9 @@ public abstract class Wagon {
      * no action if this wagon has no succeeding next wagon attached.
      */
     public void detachTail() {
-        // TODO detach this wagon from its successors (sustaining the invariant propositions).
-
+       Wagon wagon = this.nextWagon;
+        this.setNextWagon(null);
+        wagon.setPreviousWagon(null);
     }
 
     /**
@@ -143,7 +147,24 @@ public abstract class Wagon {
      * Reconnect the subsequence of its predecessors with the subsequence of its successors, if any.
      */
     public void removeFromSequence() {
-        // TODO
+        Wagon prev = this.previousWagon;
+        Wagon next = this.nextWagon;
+       if(this.hasNextWagon() && !this.hasPreviousWagon()){
+           Wagon wagon = this.nextWagon;
+           this.setNextWagon(null);
+           wagon.setPreviousWagon(null);
+       }
+
+        else if(!this.hasNextWagon() && this.hasPreviousWagon()){
+            this.previousWagon.setNextWagon(null);
+            this.setNextWagon(null);
+            this.setPreviousWagon(null);
+      }else {
+
+        this.previousWagon.setNextWagon(next);
+        this.nextWagon.setPreviousWagon(prev);
+        this.setNextWagon(null);
+        this.setPreviousWagon(null);}
 
     }
 
